7b913939758dce1b8f2121fbae13b44b6684fde3
gaetan <gbassetprotonmail.com>
2020-08-01 12:01:20 +0200

    Updates

---
+++ precache-manifest.376c590f6744d194853cf2aa8a94de8a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8cd48bd510a5de8d48915e4a1e9da951",
+    "url": "/Pokedex/index.html"
+  },
+  {
+    "revision": "71145abd252c5c47c9af",
+    "url": "/Pokedex/static/css/main.8a7c1486.chunk.css"
+  },
+  {
+    "revision": "0181721740aead6f33d9",
+    "url": "/Pokedex/static/js/2.98e15913.chunk.js"
+  },
+  {
+    "revision": "71145abd252c5c47c9af",
+    "url": "/Pokedex/static/js/main.c537d9ce.chunk.js"
+  },
+  {
+    "revision": "119fa3083960fadc0325",
+    "url": "/Pokedex/static/js/runtime~main.cf3900f6.js"
+  }
+]);
